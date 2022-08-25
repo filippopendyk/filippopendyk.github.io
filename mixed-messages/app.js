230d8c137fef77fc04b8f59339b04caade01f8d1
filippopendyk <filipos28511@gmail.com>
2022-08-24 20:51:17 -0400

    creates a function which is randomizing a joke

---
+++ mixed-messages/app.js
@@ -0,0 +1,18 @@
+// Creates a function which is returning a random joke as output
+
+const randomJoke = () => {
+
+// Creates an array of random jokes
+ const jokesArray = ["Why can't your nose be 12 inches long? Because then it'd be a foot.", "What's the best time to go to the dentist? Tooth hurty.", "What does a nut say when it sneezes? Cashew.", "When is a door not a door? When it's a-jar.", "What do you call a group of disorganized cats? A cat-tastrophe.", "How do you make a Swiss roll? Push him down a mountain.", "What do you call a person who tells dad jokes but has no kids? A faux pa.", "Why was the math book sad? Because it had so many problems.", "What did one toilet say to the other toilet? You look flushed.", "Why was the sand wet? Because the sea weed.", "What kind of dinosaur loves to sleep? A stega-snore-us.", "What's a snake's favorite subject? Hisstory.", "What do prisoners use to call each other? Cell phones.", "How does a squid go into battle? Well armed."];
+
+
+// creates randomJoke variable, which randomizes an element from jokes array
+ const randomJoke = jokesArray[Math.floor(Math.random() * 14)];
+
+//  returns a random joke
+ return randomJoke;
+};
+
+
+
+randomJoke();
