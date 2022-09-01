d64169647e427013762606f04a5e4f96fd715a2d
filippopendyk <filipos28511@gmail.com>
2022-09-01 17:16:17 -0400

    setting up tailwind

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
     },
     fontFamily: {
       'roboto': ['Roboto', 'sans-serif'],
-      'roboto-mono': ['Roboto Mono', 'monospace']
+      'roboto-mono': ['"Roboto Mono"', 'monospace']
     }
   },
   plugins: [],
