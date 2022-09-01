c5c3114fa35987d94799805f82c639bffbaef830
filippopendyk <filipos28511@gmail.com>
2022-09-01 14:22:41 -0400

    fixing syntax errors,

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'body': '#1E1E1E',
+        'selected-text': '#E94560',
+        'theme': '#E94560',
+        'nav': '##404053',
+        'secondary': '#9191A4',
+        'badge': '#3f3F51',
+        'input-border': '#061130',
+        'input': '#061130'
+      },
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'roboto-mono': ['Roboto Mono', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
